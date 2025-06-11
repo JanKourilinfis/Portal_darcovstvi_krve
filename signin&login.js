c25af5f8dcdbb2b99bb75d79e2137ed889d412e9
unknown <jan.kouril@infis.cz>
2025-06-11 09:01:34 +0200

    konecne

--- signin&login.js
+++ signin&login.js
@@ -5,11 +5,15 @@ function registrace(){
    uzivatelskyJmena.push(novýJmeno)
    var novyHeslo = document.getElementById("noveHeslo").value
    uzivatelskaHesla.push(novyHeslo)
+   alert("registrace proběhla úspěšně "+novýJmeno+" "+novyHeslo)
 }
-function prihlaseni(uzivatelskyJmena, uzivatelskaHesla){
+function prihlaseni(){
+    console.log(uzivatelskaHesla)
+    console.log(uzivatelskyJmena)
     var prihlaseniUzivatel = document.getElementById("prihlaseniJmeno").value
     var prihlaseniHesla = document.getElementById("prihlaseniHeslo").value
-    for(i of uzivatelskyJmena) {
+
+    for(var i = 0; i < uzivatelskyJmena.length; i++){ 
         if (prihlaseniUzivatel==uzivatelskyJmena[i]&&prihlaseniHesla==uzivatelskaHesla[i]){
             alert("přihlášeno")
         }
